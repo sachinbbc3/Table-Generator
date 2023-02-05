acfa2a2f95048c6946a8d713461c70b3fabfaf02
sachinbbc3 <123859331+sachinbbc3@users.noreply.github.com>
2023-02-05 13:26:28 +0400

    Add files via upload

---
+++ Table.py
@@ -0,0 +1,5 @@
+print("This is the project for generating math tables")
+n = int(input("Enter the number for printing out it's table: "))
+
+for i in range(1,11):
+    print(n, ' x ', i, ' = ', i*n)
